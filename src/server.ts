8a9590673e828a1ee3f60eb991057b6bc94f2b7d
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2022-11-05 15:50:30 -0300

    Criado autenticação e finalzado rotas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 
 import { poolRoutes } from "./routes/pool";
 import { userRoutes } from "./routes/user";
@@ -15,6 +16,10 @@ async function bootstrap() {
   await fastify.register(cors, {
     origin: true,
   });
+
+  await fastify.register(jwt, {
+    secret: 'nlwcopa'
+  })
 
   fastify.register(poolRoutes);
   fastify.register(authRoutes);
